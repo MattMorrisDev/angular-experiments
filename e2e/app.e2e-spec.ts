225471547b1167958878614fc2f686ce7273e035
Matt Morris <matt.m.morris0@gmail.com>
2017-04-25 20:43:26 -0700

    Initial app generation via cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AngularExperimentsPage } from './app.po';
+
+describe('angular-experiments App', () => {
+  let page: AngularExperimentsPage;
+
+  beforeEach(() => {
+    page = new AngularExperimentsPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
