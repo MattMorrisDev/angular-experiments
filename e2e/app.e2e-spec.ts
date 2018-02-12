8d7eeaf1dfad5aff9acaff1eb08835bc091183e7
Matt Morris <matt.m.morris0@gmail.com>
2018-02-11 17:56:54 -0800

    Upgrading more packages

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('angular-experiments App', () => {
 
    it('should display message saying app works', () => {
       page.navigateTo();
-      expect(page.getParagraphText()).toEqual('App works :)');
+      expect(page.getParagraphText()).toEqual('App works! :)');
    });
 });
